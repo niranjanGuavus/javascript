c588b589b5dec9f917a306d71c411c7a44b3d214
Niranjan swain <niranjan.litu@gmail.com>
2022-05-11 12:52:19 +0530

    this scode

--- functions.js
+++ functions.js
@@ -28,27 +28,31 @@ displayValue(); // undefined becasue for hoisting
 //Note: function expression don't take part in hoisting.
 
 /**
- *  Javascript support call;back function , i mean a function can pass as argument and 
+ *  Javascript support call;back function , i mean a function can pass as argument and
  *  that function can call in the other place. This concept call 1st order function of higer order function.
- */ 
+ */
 
- //Arrow function vs normal function
- /**
-  * It has major four difference 
-  * 1:-  syntax change. arrow function has more clean
-  * 2:- for one line statement arrow function doesn't require return statement
-  * 3:- arrow function doesn't support arguments however the normal function does.
-  * 4:- this key word:- for arriw function this refer to where it's parent scope define however in normal functio it points to it's call object.
-  */
- // Arrow function this example 
- let user =  {
-   let userName = 'Niranjan',
-   getName: ()=> this.userName,
-   getName1(){
-     return this.userName;
-   },
-  };
-  
+//Arrow function vs normal function
+/**
+ * It has major four difference
+ * 1:-  syntax change. arrow function has more clean
+ * 2:- for one line statement arrow function doesn't require return statement
+ * 3:- arrow function doesn't support arguments however the normal function does.
+ * 4:- this key word:- for arriw function this refer to where it's parent scope define however in normal functio it points to it's call object.
+ */
+// Arrow function this example
+let userName = "litu";
+let user = {
+  userName: "Niranjan",
+  getName: () => {
+    const t = () => console.log(this);
+    t();
+    return this.userName;
+  },
+  getName1() {
+    return this.userName;
+  }
+};
 
-
- }
+console.log(user.getName()); // undefine
+console.log(user.getName1());
