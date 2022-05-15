fef02446898ea44af17c420957586ec930ceabd9
Niranjan Swain <niranjan.litu@gmail.com>
2022-05-15 12:37:46 +0530

    let var const scope and it's behaviour with hostiling.

---
+++ var-let-const.js
@@ -0,0 +1,33 @@
+// var vs let vs const scoped 
+
+{
+  let a = 5;
+  var b=10;
+}
+
+//console.log(a)// reference error a is not define 
+console.log(b); // 10
+
+/**
+ * javascript execution context 
+ * 1:- creation phase. It has three phase.
+ *    a. create a window object, 
+ *    b. define a heap memory to store the variable and function. 
+ *    c. initialize those with undefined.
+ * 2:- Execution phase.
+ */
+// hoisting
+/**
+ * Temporal-dead-zone is a time between the variable declaration and initialization.
+ * */
+function abc() {
+  console.log('var scope', a); // undefined
+  //console.log(c);// Reference error c is not defines
+  var a=10
+
+  console.log('let scope', d); // ReferenceError: Cannot access 'd' before initialization
+  let d=30;
+  
+}
+
+abc()
